ac1d99db6a5f2f8d366383055abb5ea73121f4b3
Aniketh <aniketh.apr04@gmail.com>
2021-12-20 19:42:41 +0530

    implemented jump function

--- src/sample/Game.java
+++ src/sample/Game.java
@@ -1,7 +1,11 @@
 package sample;
+import javafx.animation.Interpolator;
+import javafx.animation.TranslateTransition;
+import javafx.beans.property.SimpleObjectProperty;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
@@ -15,17 +19,21 @@ import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
+import javafx.util.Duration;
+
 import java.io.File;
 import java.io.IOException;
+import java.net.URL;
 import java.util.Objects;
 import java.util.Random;
+import java.util.ResourceBundle;
 
 public class Game {
 
-    private Player Player1;
-    private Player Player2;
-    private Board GameBoard;
-    private Die Die = new Die();
+    private final SimpleObjectProperty<Player> Player1 = new SimpleObjectProperty<>(this, "player1");
+    private final SimpleObjectProperty<Player> Player2 = new SimpleObjectProperty<>(this, "player2");
+    private final Board GameBoard = new Board();
+    private final Die Die = new Die();
 
     @FXML
     public Button exitButton;
@@ -108,13 +116,41 @@ public class Game {
         String name1 = player1.getText();
         String name2 = player2.getText();
 
-        Player1 = new Player(name1, Color.BLUE, bluePawn);
-        Player2 = new Player(name2, Color.GREEN, greenPawn);
+        Player1.set(new Player(name1, Color.BLUE, bluePawn));
+        Player2.set(new Player(name2, Color.GREEN, greenPawn));
 
-        Player1.setPosition(0);
-        Player2.setPosition(0);
+        Player1.get().setPosition(0);
+        Player2.get().setPosition(0);
 
         System.out.println(name1);
         System.out.println(name2);
     }
+
+    public void jump(ActionEvent event) {
+        TranslateTransition translation = new TranslateTransition(Duration.millis(250), bluePawn);
+        translation.interpolatorProperty().set(Interpolator.SPLINE(.1, .1, .7, .7));
+        translation.setByY(-25);
+        translation.setAutoReverse(true);
+        translation.setCycleCount(2);
+//        translation.setByX(35);
+        translation.play();
+//        translation.setByX(35);
+        TranslateTransition xTransition = new TranslateTransition(Duration.millis(500), bluePawn);
+        xTransition.interpolatorProperty().set(Interpolator.SPLINE(.1, .1, .7, .7));
+        xTransition.setByX(28);
+        xTransition.play();
+    }
 }
+
+//class PawnController implements Initializable {
+//
+//    @Override
+//    public void initialize(URL url, ResourceBundle resourceBundle) {
+//        TranslateTransition translation = new TranslateTransition(Duration.millis(500), this);
+//        translation.interpolatorProperty().set(Interpolator.SPLINE(.1, .1, .7, .7));
+//        translation.setByY(-50);
+//        translation.setAutoReverse(true);
+//        translation.setCycleCount(2);
+//        translation.play();
+//    }
+//}
