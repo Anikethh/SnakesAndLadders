96ea23c89b6b0e380812b336bdd4201e785bc87f
Anshul <anshul20361@iiitd.ac.in>
2021-12-21 19:56:34 +0530

    Sample fxml fix

---
+++ src/sample/gamecont2.java
@@ -0,0 +1,16 @@
+package sample;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class gamecont2 {
+    @FXML
+    Label name1;
+    Label name2;
+
+    public void disname(String playername1,String playername2){
+        name1.setText(playername1);
+        name2.setText(playername2);
+    }
+
+}
